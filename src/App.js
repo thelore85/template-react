dba18f80962f8abaee46ea329e242a90787cc6ae
TheLore85 <garofalo.digital@gmail.com>
2023-07-05 19:15:17 +0200

    webpack upgrated with support for: css, scss, svg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component{
 	render(){
 		return(
 			<div className='app-container'>
 				<h1>BUILD YOUR OWN APP</h1>
+				<p>This is the first react template created only with WebPack</p>
 			</div>
 			
 		)
--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,30 @@ module.exports ={
 	entry:'./src/index.js',
 	output:{
 		path: path.join(__dirname, '/build'),
-		filename: 'boundle.js',
+		filename: 'bundle.js',
 	},
+	devServer:{
+		port: 1985,
+	},
+
 	plugins:[
 		new HTMLWebpackPlugin({ template: './src/index.html'})
 	],
 	module: {
-		rules: [{ 
-			test: /.js$/, 
+		rules: [
+			{ 
+			test: /\.(js|jsx)$/, //manage .js and .jsx
 			exclude: /node_modules/,
 			use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/preset-react'] }}
-		}]
+			},
+			{
+			test:/\.(scss|css)$/, // manage .css and .scss
+			use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{
+			test:/\.svg$/, //manage .svg file
+			use:[{loader:'svg-url-loader', options:{limiit:10000,}}]
+			},
+		]
 	}
 }

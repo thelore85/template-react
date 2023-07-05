4e42d034d269096f63ecc6964d0f3fd282fdf955
TheLore85 <garofalo.digital@gmail.com>
2023-07-05 14:04:08 +0200

    first file update

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports ={
+	entry:'./src/index.js',
+	output:{
+		path: path.join(__dirname, '/build'),
+		filename: 'boundle.js',
+	},
+	plugins:[
+		new HTMLWebpackPlugin({ template: './src/index.html'})
+	],
+	module: {
+		rules: [{ 
+			test: /.js$/, 
+			exclude: /node_modules/,
+			use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/preset-react'] }}
+		}]
+	}
+}

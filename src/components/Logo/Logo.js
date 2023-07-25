b94caa7106cc9567daeb9ce8a96e7b72e942debc
TheLore85 <garofalo.digital@gmail.com>
2023-07-26 00:41:37 +0200

    menu links css

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+  return(
+    <div id="logo" className="logo">
+      <i class="fa-solid fa-mug-hot"></i>
+      MY-LOGO
+    </div>
+  )
+}
+
+export default Logo;
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,23 +1,21 @@
 import React from 'react';
+import Logo from '../Logo/Logo.js';
 import './Menu.css';
 
 
 const Menu = () => {
+  return(
+    <section id="menu">
+      <div className="wrapper">
+        <Logo />
 
-    return(
-        <section id="menu">
-            <div className="wrapper">
-                <div className="logo">
-                  <i class="fa-solid fa-mug-hot"></i>
-                  MY-LOGO
-                </div>
-                <div className="links">
-                    <a href="#">Menu</a>
-                    <a href="#">Log-in</a>   
-                </div>
-            </div>
-        </section>
-    )
+        <div className="links">
+            <a href="#">Register</a>
+            <a href="#">Log-in</a>   
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Menu;
